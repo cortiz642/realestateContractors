24ad93527e7b5b57fc81e3c1e106f77d21135d0a
cortiz642 <chris.o.ortiz93@gmail.com>
2022-08-13 23:17:39 -0500

    test

---
+++ src/main/java/com/contractors/realestatecontractors/RealestateContractorsApplication.java
@@ -0,0 +1,13 @@
+package com.contractors.realestatecontractors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateContractorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealestateContractorsApplication.class, args);
+    }
+
+}
